b5b8f0f20f1d646c043fee683fd19a374558c10b
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2024-07-03 11:54:29 -0400

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -4,7 +4,7 @@ define('DB_USERNAME', 'dvwa');
 define('DB_PASSWORD', 'password');
 define('DB_DATABASE', 'dvwa');
 define('DVWA_WEB_PAGE_TO_ROOT', '');
-define('DVWA_DEFAULT_SECURITY_LEVEL', 'impossible');
+define('DVWA_DEFAULT_SECURITY_LEVEL', 'low');
 define('DVWA_RECAPTCHA_PRIVATE_KEY', '');
 define('DVWA_RECAPTCHA_PUBLIC_KEY', '');
 define('PHPIDS_ENABLED', false);
