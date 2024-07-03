e5cd5617d529aa85bf7fb51c31238dd80e2a856b
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2024-07-03 12:44:22 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,6 +7,7 @@ resource "aws_subnet" "dvwa_subnet" {
   vpc_id            = aws_vpc.dvwa_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "dvwa_igw" {
