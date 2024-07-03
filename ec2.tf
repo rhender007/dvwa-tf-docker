87f1d8277e8fcbb28c360870a7957f56cd8631f2
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2024-07-03 12:01:07 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,26 @@
 # ec2.tf
 resource "aws_instance" "dvwa_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS AMI
-  instance_type = "t2.large"
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
   subnet_id     = aws_subnet.dvwa_subnet.id
   security_groups = [aws_security_group.dvwa_sg.name]
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt update
-              sudo apt install -y docker.io docker-compose
-              sudo systemctl start docker
-              sudo systemctl enable docker
+              sudo yum update -y
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              sudo chkconfig docker on
+
+              # Install Docker Compose
+              sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
 
               # Clone DVWA and run with Docker Compose
-              git clone https://github.com/your_github_username/terraform-dvwa.git /home/ubuntu/terraform-dvwa
-              cd /home/ubuntu/terraform-dvwa
-              sudo docker-compose up -d
+              git clone https://github.com/your_github_username/terraform-dvwa.git /home/ec2-user/terraform-dvwa
+              cd /home/ec2-user/terraform-dvwa
+              sudo /usr/local/bin/docker-compose up -d
               EOF
 
   tags = {
