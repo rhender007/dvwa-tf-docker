f9c292e0c9a9d7ff2e1d90248f42c8e85e2883f5
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2024-07-03 12:29:54 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,8 @@ resource "aws_instance" "dvwa_instance" {
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
-              sudo amazon-linux-extras install docker -y
+              # sudo amazon-linux-extras install docker -y
+              sudo yum install docker -y
               sudo service docker start
               sudo usermod -a -G docker ec2-user
               sudo chkconfig docker on
