073b02566fde5fb17d1e5c038dc5df2617482a5a
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2024-07-03 11:44:01 -0400

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_SERVER', 'dvwa_db');
+define('DB_USERNAME', 'dvwa');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'dvwa');
+define('DVWA_WEB_PAGE_TO_ROOT', '');
+define('DVWA_DEFAULT_SECURITY_LEVEL', 'impossible');
+define('DVWA_RECAPTCHA_PRIVATE_KEY', '');
+define('DVWA_RECAPTCHA_PUBLIC_KEY', '');
+define('PHPIDS_ENABLED', false);
+define('PHPIDS_VERBOSE', false);
+define('PHPIDS_LOG_FILE', '/var/log/phpids_log.txt');
+?>
