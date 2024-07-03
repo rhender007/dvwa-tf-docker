793c070816ac75f9262a266a5f555aa1d81842e4
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2024-07-03 13:54:06 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "dvwa_instance" {
               sudo yum install python -y
               sudo yum install git -y
               sudo yum install openssl openssl-devel -y
+              sudo yum install libxcrypt-compat -y
               # sudo amazon-linux-extras install docker -y
               sudo yum install docker -y
               sudo service docker start
