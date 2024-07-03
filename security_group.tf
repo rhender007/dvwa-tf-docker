7f84bdefb84e28f96ca8968026603b4778f8722b
Robert Henderson <35784314+rhender007@users.noreply.github.com>
2024-07-03 11:38:28 -0400

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,25 @@
+# security_group.tf
+resource "aws_security_group" "dvwa_sg" {
+  vpc_id = aws_vpc.dvwa_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
